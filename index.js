327d87402ef28d243194d495f7226d961f871616
mamondots <shikderr112@gmail.com>
2023-06-09 22:57:33 +0600

    add my seleted get api and post api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { send } = require('express/lib/response');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -29,6 +30,7 @@ async function run() {
 
 
     const classesCollection = client.db('SummerCamp').collection('Classes')
+    const seletedCollection = client.db('SummerCamp').collection('seleted')
     const instructorCollection = client.db('SummerCamp').collection('Instructors')
 
     app.get('/classes', async(req, res) =>{
@@ -48,6 +50,25 @@ async function run() {
       const result = await classesCollection.insertOne(newItem)
       res.send(result)
   })
+
+  //seletedclasses
+
+  app.get('/seleted', async (req, res) =>{
+    const email = req.query.email
+    if(!email){
+      res.send([])
+    }
+    const query = {email: email}
+    const result = await seletedCollection.find(query).toArray()
+    res.send(result)
+  })
+
+  app.post('/seleted', async (req, res) => {
+    const seletedItem = req.body
+    const result = await seletedCollection.insertOne(seletedItem)
+    res.send(result)
+})
+   
 
     app.get('/instructors', async(req, res) =>{
         const result = await instructorCollection.find().toArray()
