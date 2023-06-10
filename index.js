8658910c7b17195c82470e86777c7fe04869c490
mamondots <shikderr112@gmail.com>
2023-06-10 17:06:34 +0600

    create admin and instruct api

--- index.js
+++ index.js
@@ -25,61 +25,125 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
- 
+
     await client.connect();
 
 
+    const usersCollection = client.db('SummerCamp').collection('users')
     const classesCollection = client.db('SummerCamp').collection('Classes')
     const seletedCollection = client.db('SummerCamp').collection('seleted')
     const instructorCollection = client.db('SummerCamp').collection('Instructors')
 
-    app.get('/classes', async(req, res) =>{
-        const result = await classesCollection.find().toArray()
-        res.send(result)
+
+    //for user
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray()
+      res.send(result)
     })
 
-    app.get('/classes/:id', async(req, res) =>{
-        const id = req.params.id
-        const query = {_id : new ObjectId(id)}
-        const result = await classesCollection.findOne(query)
-        res.send(result)
+    app.post('/users', async (req, res) => {
+      const user = req.body
+      console.log(user)
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'User Already Have!' })
+      }
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = {_id : new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      }
+      const result = await usersCollection.updateOne(filter,updateDoc)
+      res.send(result)
+    })
+
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = {_id : new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
+        },
+      }
+      const result = await usersCollection.updateOne(filter,updateDoc)
+      res.send(result)
+    })
+
+    //for classes
+    app.get('/classes', async (req, res) => {
+      const query = {}
+      const options = {
+        sort: { "available_seats": -1 }
+      }
+      const result = await classesCollection.find(query, options).toArray()
+      res.send(result)
+    })
+
+    app.get('/classes/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await classesCollection.findOne(query)
+      res.send(result)
     })
 
     app.post('/classes', async (req, res) => {
       const newItem = req.body
       const result = await classesCollection.insertOne(newItem)
       res.send(result)
-  })
+    })
 
-  //seletedclasses
+    app.delete('/classes/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await classesCollection.deleteOne(query)
+      res.send(result)
+    })
 
-  app.get('/seleted', async (req, res) =>{
-    const email = req.query.email
-    if(!email){
-      res.send([])
-    }
-    const query = {email: email}
-    const result = await seletedCollection.find(query).toArray()
-    res.send(result)
-  })
+    //seletedclasses
 
-  app.post('/seleted', async (req, res) => {
-    const seletedItem = req.body
-    const result = await seletedCollection.insertOne(seletedItem)
-    res.send(result)
-})
-   
+    app.get('/seleted', async (req, res) => {
+      const email = req.query.email
+      if (!email) {
+        res.send([])
+      }
+      const query = { email: email }
+      const result = await seletedCollection.find(query).toArray()
+      res.send(result)
+    })
 
-    app.get('/instructors', async(req, res) =>{
-        const result = await instructorCollection.find().toArray()
-        res.send(result)
+    app.post('/seleted', async (req, res) => {
+      const seletedItem = req.body
+      const result = await seletedCollection.insertOne(seletedItem)
+      res.send(result)
+    })
+
+    app.delete('/seleted/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await seletedCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
+    app.get('/instructors', async (req, res) => {
+      const result = await instructorCollection.find().toArray()
+      res.send(result)
     })
 
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
- 
+
     //await client.close();
   }
 }
@@ -87,10 +151,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res) =>{
-    res.send('summer comp is running')
+app.get('/', (req, res) => {
+  res.send('summer comp is running')
 })
 
-app.listen(port, () =>{
-    console.log(`summer comp is running on port, ${port}`)
+app.listen(port, () => {
+  console.log(`summer comp is running on port, ${port}`)
 })
