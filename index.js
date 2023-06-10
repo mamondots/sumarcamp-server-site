4d8aa97c4f25d1dcac7062cb357364423a83c03a
mamondots <shikderr112@gmail.com>
2023-06-10 20:46:47 +0600

    make the api

--- index.js
+++ index.js
@@ -78,6 +78,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await usersCollection.deleteOne(query)
+      res.send(result)
+    })
+
     //for classes
     app.get('/classes', async (req, res) => {
       const query = {}
